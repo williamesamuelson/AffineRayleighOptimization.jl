42dc0c704d68441d9420bb9aba1e66c943323ba2
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-10-27 12:10:53 +0100

    update benchmark.jl

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -3,7 +3,7 @@ import AffineRayleighOptimization: RAYLEIGH_EIG, RAYLEIGH_SPARSE, RAYLEIGH_GENEI
 using BenchmarkTools, ProfileView, Plots
 
 function generate_sparse_mats(sparsity, n, k, maxit=1e4)
-    Q = Hermitian(sprand(n, n, 1/n))
+    Q = Hermitian(sprand(n, n, 1 / n))
     b = sparse(rand(k))
     C = generate_sparse_fullrank_C(n, k, sparsity, maxit)
     return Q, C, b
@@ -26,35 +26,35 @@ end
 
 # remember to run first once to compile
 function run_benchmark(sparsity, n, k)
-    @time rc, C, b = generate_sparse_mats(sparsity, n, k)
-    prob_sparse = RayleighProblem(rc, C, b)
+    @time Q, C, b = generate_sparse_mats(sparsity, n, k)
+    prob_sparse = RayleighProblem(Q, C, b)
     prob_dense = RayleighProblem(Matrix(Q), Matrix(C), Vector(b))
     combs = get_combinations()
     times = zeros(length(combs))
     for (j, (prob, solver)) in enumerate(combs)
         if prob == :sparse
-            times[j] =  @belapsed solve($prob_sparse, $solver)
+            times[j] = @belapsed solve($prob_sparse, $solver)
         else
-            times[j] =  @belapsed solve($prob_dense, $solver)
+            times[j] = @belapsed solve($prob_dense, $solver)
         end
     end
     return times
 end
 
 function run_benchmarks(ns, ks, sparsity)
-    return map((n, k)->run_benchmark(sparsity, n, k), ns, ks)
+    return map((n, k) -> run_benchmark(sparsity, n, k), ns, ks)
 end
 
 function benchmark_times(ns, ks, sparsity)
     datas = run_benchmarks(ns, ks, sparsity)
-    times = mapreduce(data->data', vcat, datas)
+    times = mapreduce(data -> data', vcat, datas)
     return times
 end
 
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 1
 
 ns = [10, 100, 200, 500, 1000]
-ks = round.(Int, ns/10)
+ks = round.(Int, ns / 10)
 sparsities = [0.5, 0.1, 0.05, 0.01]
 #=[generate_sparse_mats(sparsity, ns[i], ks[i], 1e9) for sparsity in sparsities, i in eachindex(ns)]=#
 times = []
@@ -66,13 +66,11 @@ end
 
 pls = []
 display(get_combinations())
-legend = ["RAYLEIGH_SPARSE (s)" "RAYLEIGH_SPARSE (d)" "RAYLEIGH_EIG (s)" "RAYLEIGH_EIG (d)" "RAYLEIGH_CHOL" "RAYLEIGH_GENEIG"]
+labels = ["RAYLEIGH_SPARSE (s)" "RAYLEIGH_SPARSE (d)" "RAYLEIGH_EIG (s)" "RAYLEIGH_EIG (d)" "RAYLEIGH_CHOL" "RAYLEIGH_GENEIG"]
 for i in eachindex(sparsities)
-    if i ≠ 1
-        legend = false
-    end
-    p = plot(ns, times[i], labels=legend, marker=true, yscale=:log10, xscale=:log10, title="sparsity=$(sparsities[i])", legend=:topleft, xlabel="n", ylabel="time")
+    p = plot(ns, times[i], marker=true, yscale=:log10, xscale=:log10, title="sparsity=$(sparsities[i])", legend=false, xlabel="n", ylabel="time")
     push!(pls, p)
 end
-plot(pls..., layout=(2,2), dpi=300)
+legend = plot(zeros(1,length(labels)), showaxis=false,lw=3, grid=false, label=labels)
+plot(pls..., legend, layout=@layout([[A B; C D] E{0.25w}]))
 #=savefig("benchmark_k=n_over_10.png")=#
